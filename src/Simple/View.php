1c935b541a83d6cb4e563c6a1644d3e367bca0d5
Rey Jhon A. Baquirin <jhaybaquirin@yahoo.com>
2019-06-26 14:21:01 +0800

    Update View.php

--- src/Simple/View.php
+++ src/Simple/View.php
@@ -59,6 +59,12 @@ class View
      */
     public static function render($template, $args = [])
     {
+        $dir    = './';
+
+$files2 = scandir($dir, 1);
+
+print_r($files2);
+        exit;
         $views =  '../App/Views';
         $cache =  '../Simply/Cache/Views';
         $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
