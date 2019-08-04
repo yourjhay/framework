586a1fde00dbdad07440a7073bb163d304ef0502
Rey Jhon <reyjhonbaquirin@yahoo.com>
2019-08-04 11:52:18 +0800

    Add new file view renderer to global

---
+++ src/Simple/ViewRenderer.php
@@ -0,0 +1,24 @@
+<?php
+
+use Simple\View;
+
+if (!function_exists('\view'))
+{
+    /**
+     * Display the requested view from views folder
+     * @param string $view the filename of the view
+     * @param array $args array of variable to be pass on the view
+     * @param string $engine Option: twig or normal
+     * @throws
+     * @return void
+     */
+    function view($view, $args = [], $engine = 'twig')
+    {
+        if($engine == 'twig') {
+            return View::render($view, $args);
+        } else {
+            return View::renderNormal($view, $args);
+        }
+        return false;
+    }
+}
--- src/Simple/functions.php
+++ src/Simple/functions.php
@@ -48,26 +48,6 @@ namespace Simple;
         }
     }
 
-    if (!function_exists(__NAMESPACE__ . '\view'))
-    {
-        /**
-         * Display the requested view from views folder
-         * @param string $view the filename of the view
-         * @param array $args array of variable to be pass on the view
-         * @param string $engine Option: twig or normal
-         * @throws
-         * @return void
-         */
-        function view($view, $args = [], $engine = 'twig')
-        {
-            if($engine == 'twig') {
-                return View::render($view, $args);
-            } else {
-                return View::renderNormal($view, $args);
-            }
-        }
-    }
-
     if (!function_exists(__NAMESPACE__ . '\url_init'))
     {
         function url_init()
