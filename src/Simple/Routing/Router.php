b6b1080b6d54b53c66c16fd5bdd556f07ee84fbd
R-Jhay <reyjhonbaquirin@yahoo.com>
2020-05-17 18:55:54 +0800

    added new feature to Router

--- src/Simple/Routing/Router.php
+++ src/Simple/Routing/Router.php
@@ -1,46 +1,76 @@
 <?php
 namespace Simple\Routing;
 
-class Router
+class Router Extends BaseRouter
 {
     /**
-     * Associative array of register routes
-     * @var array
+     * SET Route to accept only POST method
      */
-    protected static $routes = [];
-
-    /**
-     * Parameters from matched routes
-     * @var array
-     */
-    protected static $params = [];
-
-    /**
-     * register routes
-     * @param string $route - Route URL
-     * @param array $params Parameters (controller, action, etc.)
-     */
-    public static function set($route, $params = [])
+    public static function post($route, $params = [])
     {
+        parent::set($route, $params, 'POST');
+        return new static;
+    }
 
-        //convert the route to a regular exp. escape forward slashes
-        $route = preg_replace('/\//','\\/', $route);
+    /**
+     * SET Route to accept only GET method
+     */
+    public static function get($route, $params = [])
+    {     
+        parent::set($route, $params, 'GET');
+        return new static;
+    }
 
-        //convert var like {controller}
-        $route = preg_replace('/\{([a-z]+)\}/','(?P<\1>[a-z-]+)', $route);
+    /**
+     * SET Route to accept only PUT method
+     */
+    public static function put($route, $params = [])
+    {
+        parent::set($route, $params, 'PUT');
+        return new static;
+    }
 
-        //convert optional variable
-        $route = preg_replace('/\{([a-z]+)([?]+)\}/', '(?P<\1>\w+)\2', $route);
+    /**
+     * SET Route to accept only DELETE method
+     */
+    public static function delete($route, $params = [])
+    {
+        parent::set($route, $params, 'DELETE');
+        return new static;
+    }
 
-        //convert variables with custom regex eg: {id: \d+}
-        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+    /**
+     * SET Route to accept only PATCH method
+     */
+    public static function patch($route, $params = [])
+    {
+        parent::set($route, $params, 'PATCH');
+        return new static;
+    }
 
-        //this convert {:all?} to accept any url pass through
-        $route = preg_replace('/\{([:]+)([all]+)([?]+)\}/', '(?P<\2>([a-z0-9\/-]+))\3', $route);
+    /**
+     * SET Route to accept only PATCH method
+     */
+    public static function any($route, $params = [])
+    {
+        parent::set($route, $params, 'ANY');
+        return new static;
+    }
 
-        //add start and end delimiters, case insensitive flag
-        $route = '/^' . $route . '$/i';
-        self::$routes[$route] = $params;
+    /**
+     * SET Route to accept only PUT method
+     */
+    public static function head($route, $params = [])
+    {
+        parent::set($route, $params, 'HEAD');
+        return new static;
+    }
+
+    public static function group(string $prefix, callable $routes)
+    {
+        $prevGroupPrefix = parent::$currentGroupPrefix;
+        parent::$currentGroupPrefix = $prefix . $prevGroupPrefix;
+        call_user_func($routes);
     }
 
     /**
@@ -49,180 +79,32 @@ class Router
      */
     public static function resource($route, $controller)
     {
-        self::set($route, ['controller' => $controller, 'action' => 'index']);
-        self::set("$route/store", ['controller' => $controller, 'action' => 'store']);
-        self::set("$route/create", ['controller' => $controller, 'action' => 'create']);
-        self::set("$route/edit/{id:\w+}", ['controller' => $controller, 'action' => 'edit']);
-        self::set("$route/update/{id:\w+}", ['controller' => $controller, 'action' => 'update']);
-        self::set("$route/show/{id:\w+}", ['controller' => $controller, 'action' => 'show']);
-        self::set("$route/destroy/{id:\w+}", ['controller' => $controller, 'action' => 'destroy']);
+        self::get($route, ['controller' => $controller, 'action' => 'index']);
+        self::post("$route/store", ['controller' => $controller, 'action' => 'store']);
+        self::get("$route/create", ['controller' => $controller, 'action' => 'create']);
+        self::get("$route/edit/{id:\w+}", ['controller' => $controller, 'action' => 'edit']);
+        self::put("$route/update/{id:\w+}", ['controller' => $controller, 'action' => 'update']);
+        self::get("$route/show/{id:\w+}", ['controller' => $controller, 'action' => 'show']);
+        self::delete("$route/destroy/{id:\w+}", ['controller' => $controller, 'action' => 'destroy']);
     }
 
     public static function auth()
     {
-        self::set('auth/{action:\bindex|\bauthenticate|\blogout}',[
-            'controller' => 'AuthController',
-            'namespace' => 'Auth'
-        ]);
+        self::get('auth/','Auth\Auth@index')
+            ->alias('auth.index');
 
-        self::set('auth/{action:\bSignup|\bsignup-new}',[
-            'controller' => 'SignupController',
-            'namespace' => 'Auth'
-        ]);
+        self::get('auth/logout','Auth\Auth@logout')
+            ->alias('auth.logout');
 
-        self::set('{controller:password}/{action:\breset|\bemail}/{token?}');
+        self::post('auth/authenticate','Auth\Auth@authenticate')
+            ->alias('auth.authenticate');
+
+        self::get('auth/signup','Auth\Signup@signup')
+            ->alias('auth.signup');
+
+        self::post('auth/signup-new','Auth\Signup@signup-new')
+            ->alias('auth.signup-new');
+
+        //self::set('{controller:password}/{action:\breset|\bemail}/{token?}');
     }
-
-    /**
-     * Return all available routes
-     * @return array
-     */
-    public static function getRoutes()
-    {
-        return self::$routes;
-    }
-
-    /**
-     * Match the route to $routes setting $params property
-     * if a route is found
-     * @param string $url - The route URL
-     * @return boolean true if match found else false
-     */
-    public static function match($url)
-    {
-        foreach(self::$routes as $route => $params){
-            if(preg_match($route, $url, $matches)){
-                foreach($matches as $key => $match){
-                    if(is_string($key)){
-                        $params[$key] = $match;
-                    }
-                }
-                self::$params = $params;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Get matched parameters
-     */
-    public static function getParams()
-    {
-        return self::$params;
-    }
-
-    /**
-     * Dispatch route parameter to URL
-     *
-     */
-    public static function dispatch($url)
-    {
-        $url_orig = $url;
-        $retry = 0;
-
-        $url = self::removeQueryString($url);
-
-        tryagain:
-
-        if(self::match($url)) {
-            if (preg_match('/controller$/i', self::$params['controller']) == 0) {
-                $controller = self::$params['controller'].'Controller';
-            } else {
-                $controller = self::$params['controller'];
-            }
-            $controller = self::convertToStudlyCaps($controller);
-            $controller = self::getNamespace() . $controller;
-
-            if(class_exists($controller)){
-
-                $controller_object = new $controller(self::$params);
-                $action = self::$params['action'];
-                $action = self::convertToCamelCase($action);
-
-                if (preg_match('/action$/i', $action) == 0) {
-
-                    echo $controller_object->$action(new \Simple\Request);
-
-                } else {
-                    throw new \Exception("Method [$action] (in Controller [$controller] ) can't be called explicitly. Remove Action suffix instead");
-                }
-            } else {
-                throw new \Exception("Controller class [$controller] not found");
-            }
-        } else {
-            if($retry > 0){
-                throw new \Exception("INVALID ROUTE [$url]", 404);
-            } else {
-
-                if(substr($url,-1) == '/' && $retry==0) {
-                    $url = substr($url,0,-1);
-                    $retry+=1;
-                } else {
-                    $url = $url_orig.'/';
-                    $retry+=1;
-                }
-                goto tryagain;
-            }
-        }
-    }
-
-    /**
-     * convert string into Studly Case format
-     * @var string
-     * @return string
-     */
-    private static function convertToStudlyCaps($string)
-    {
-        return str_replace(' ','',ucwords(str_replace('-',' ', $string)));
-    }
-
-    /**
-     * convert string into Camel Case format
-     * @var string
-     * @return string
-     */
-    private static function convertToCamelCase($string)
-    {
-        return lcfirst(self::convertToStudlyCaps($string));
-    }
-
-    /**
-     * Remove query string from url like:
-     * ?page=1&id=1...... etc.
-     * @param string $url
-     * @return array or string URL
-     */
-    protected static function removeQueryString($url)
-    {
-        if($url != ''){
-            $parts = explode('&', $url, 2);
-            if(strpos($parts[0], '=') === false){
-                $url = $parts[0];
-            } else {
-                
-                if(strpos($parts[0], '?') === false){
-                    $url = '';
-                } else {
-                    $url = explode('?',$parts[0]);
-                }
-                
-            }
-        }
-        return is_array($url)?$url[0]:$url;
-    }
-
-    /**
-     * Getnamespace in route params to specify where it has to be called
-     * @return string $namespace
-     */
-    protected static function getNamespace()
-    {
-        $namespace = 'App\Controllers\\';
-        if(array_key_exists('namespace', self::$params)) {
-            $namespace .= self::$params['namespace'] . '\\';
-        }
-        return $namespace;
-    }
-
 }
