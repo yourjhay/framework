bcceecb6bc4a0f9de6fd44bf0135d1213372845b
Rey Jhon Baquirin <reyjhonbaquirin@yahoo.com>
2021-05-23 11:04:01 +0800

    Update git ignore and add test model file

---
+++ test/app/Model.php
@@ -0,0 +1,40 @@
+<?php
+namespace Simple\Tests\app;
+
+/**
+ * Application Configuration Settings
+ */
+define('APP_NAME', 'Rey Jhon : Me');
+define('APP_DESCRIPTION', 'The "Simply-PHP" Framework');
+define('BASEURL', '');
+define('APP_KEY', '');
+
+/**
+ * Error handling behaviour
+ * Set to false in production
+ */
+define('SHOW_ERRORS', true);
+
+/**
+ * Options:
+ *  simply - use the default error handling template.
+ *  whoops - use "filp/whoops" error handling library.
+ *  **IF you set whoops as default ERROR_HANDLER you need to install it.
+ *    run: composer require filp/whoops
+ */
+define('ERROR_HANDLER', 'whoops');
+
+define('DBENGINE', 'mysql');
+
+define('DBSERVER', 'localhost');
+define('DBUSER', 'jhay');
+define('DBPASS', 'password');
+define('DBNAME', 'portfolio');
+
+
+
+class Model extends \Simple\Model
+{
+    protected array $fillable =['fn'];
+    protected string $table = 'users';
+}
