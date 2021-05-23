18c03885afc6e44a73db7340d23050ea1fdf1a91
Rey Jhon Baquirin <reyjhonbaquirin@yahoo.com>
2021-05-23 11:01:06 +0800

    Update model test

--- test/ModelTest.php
+++ test/ModelTest.php
@@ -15,32 +15,4 @@ class ModelTest extends \PHPUnit\Framework\TestCase
          $model->fn = 'Jhay            ';
          $this->assertEquals($model->fn,'Jhay');
      }
-
-     public function testQueryBuilderWhere()
-     {
-         $model = new \Simple\Tests\app\Model;
-         $response = $model->where('id','6')->get();
-         $response = $response[0];
-         $this->assertIsArray($response);
-         $this->assertArrayHasKey('id',$response);
-         $this->assertArrayHasKey('email',$response);
-         $this->assertArrayHasKey('name',$response);
-
-
-         $this->assertEquals($response['email'], 'jhay@yahoo.com');
-         $this->assertEquals($response['name'], 'Rey Jhon Baquirin');
-         $this->assertEquals($response['id'], 6);
-
-     }
-
-     public function testQueryBuilderOnly()
-     {
-         $model = new \Simple\Tests\app\Model;
-         $response = $model->only('6');
-         $this->assertIsObject($response);
-         $this->assertInstanceOf("\\Simple\\Tests\\app\\Model", $response);
-
-         $response = $model->only('7');
-         $this->assertIsBool($response);
-     }
 }
